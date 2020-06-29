a7460524027f905dd94f6aa51281cab11199e20e
Xwilarg <xwilarg@yahoo.fr>
2020-06-30 01:37:17 +0200

    Add empty Discord bot

---
+++ Tami/Modules/Communication.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Commands;
+using DiscordUtils;
+using System.Threading.Tasks;
+
+namespace Tami.Modules
+{
+    public class Communication : ModuleBase
+    {
+        [Command("Info")]
+        public async Task Info()
+        {
+            await ReplyAsync(embed: Utils.GetBotInfo(Program.P.StartTime, "Tami", Program.P.Client.CurrentUser));
+        }
+    }
+}
